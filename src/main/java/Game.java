d2ac07cd8aea244e4951f1156b5435368e3e22a1
tjhexf <tjhexf@proton.me>
2024-08-06 22:20:23 -0300

    Added top Panel Layout, so you can switch between game states

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,11 +5,16 @@ import java.io.IOException;
 public class Game extends JFrame {
     public Game() throws IOException, FontFormatException {
         super("Java Dungeon");
+        JPanel cardPanel = new JPanel(new CardLayout());
         setSize(800, 800);
+        add(cardPanel);
+
         CharSel charSel = new CharSel();
-        add(charSel);
+        cardPanel.add(charSel, "Character Selection");
+
         charSel.run();
         setVisible(true);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }

b3f1be7516c13b85ccdd620b18cef1f71b518cb5
Kauê Bosel <139825759+kauebbosel@users.noreply.github.com>
2024-08-24 16:51:09 -0300

    Delete jogo-poo-java-main directory

--- jogo-poo-java-main/src/main/java/Armadilha.java
+++
@@ -1,9 +0,0 @@
-import java.util.Random;
-
-public class Armadilha extends Estrutura{
-    private boolean tipo;
-
-    public void tiraPontos(/*não entendi o diagrama, mas aqui fica um tipo*/){
-
-    }
-}
